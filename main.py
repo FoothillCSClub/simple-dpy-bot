74f669ffa897b4bb5029d92826816bd00e56afd4
Xithrius <15021300+Xithrius@users.noreply.github.com>
2020-12-28 17:08:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,35 @@
 from discord.ext import commands
 
-import aiohttp
-
-from random import randint
-import pprint
 from os import getenv
 
+import aiohttp
+
+from random import choice
+
 bot = commands.Bot(
-    command_prefix='prefix '
+    command_prefix='charles '
 )
-# 5
-bot.http_session = aiohttp.ClientSession()
-
-# 1
-
 
 @bot.event
 async def on_ready():
-    print(f'Bot {bot.user.name} with user ID {bot.user.id} is ready to use!')
+    print(f'Bot logged in as {bot.user.name}')
 
-# 3
-
-
-@bot.event
-async def on_message(message):
-    if 'heck' in message.content:
-        await message.channel.send('That is not appropriate!')
-
-# 2
-
+    bot.http_session = aiohttp.ClientSession()
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello!')
-
-# 4
-
-
-@bot.command()
-async def roll(ctx, amount: int):
-    roles = [randint(1, 6) for _ in range(amount)]
-
-    await ctx.send(f'Dice rolled {amount} times. Average: {sum(roles) / amount}.')
-
-# 6
-
+    await ctx.send(f'Hello, {ctx.author.mention}')
 
 @bot.command()
 async def reddit(ctx, subreddit):
-    async with bot.http_session.get('https://www.reddit.com/r/Eyebleach/top/.json?t=day') as resp:
+    async with bot.http_session.get(f'https://www.reddit.com/r/{subreddit}/top/.json?t=day&limit=10') as resp:
         if resp.status != 200:
-            await ctx.send(f'Something went wrong! Status code {resp.status}')
+            await ctx.send(f'Oh no, an error occured! Status code: {resp.status}.')
             return
-        else:
-            json = await resp.json()
-            pprint(json)
 
-bot.run(getenv('TOKEN'), bot=True, reconnect=True)
+        json = await resp.json()
+        data = json['data']['children']
+        await ctx.send(choice(data)['data']['url'])
+
+
+bot.run(getenv('TOKEN'))
